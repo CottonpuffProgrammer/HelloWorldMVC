4386705f5b6321d556ea0859a534ee6ac2a398ba
Isaac Detert <detert7392@students.cptc.edu>
2024-07-11 23:19:32 -0700

    Initial Commit

--- AlmostEmptyStarter/Controllers/HomeController.cs
+++ AlmostEmptyStarter/Controllers/HomeController.cs
@@ -4,7 +4,17 @@ namespace AlmostEmptyStarter.Controllers
 {
     public class HomeController : Controller
     {
+        // Home/Index
         public IActionResult Index()
+        {
+            /* Sets Title to Viewdata, optional if
+             * already present elsewhere in code
+            ViewData["Title"] = "Home Page";
+            */
+            return View();
+        }
+
+        public IActionResult About()
         {
             return View();
         }
